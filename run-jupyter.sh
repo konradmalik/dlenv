0ad799b08a7b655c486346fe2b5257960021288e
Konrad Malik <konrad.malik@gmail.com>
2019-06-30 22:30:22 +0200

    expose spark and mlflow ports

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -4,5 +4,7 @@ docker run --rm -it \
     --name dlenv \
     --volume /home/konrad/Software/DLEnv/data:/data \
     --publish 8888:8888 \
-    --ipc host \
+	--publish 5000:5000 \
+	--publish 4040:4040 \
+	--ipc host \
 	dlenv jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token= --notebook-dir='/root'
