5607ffc6f1bb29e27b16d7603aa995e99534952f
Konrad Malik <konrad.malik@gmail.com>
2019-11-10 16:01:39 +0100

    disable org logger by default

--- scripts/almond-install.sh
+++ scripts/almond-install.sh
@@ -2,7 +2,7 @@
 
 # need to override hadoop to support http!
 # we will filter out 'curator' jars as they are then reimported with new hadoop
-
+# also disable logs for 'org' to not pollute all notebook
 ./almond --install --global \
 --predef-code " 
   val jars = java.nio.file.Files.list(java.nio.file.Paths.get(\"${SPARK_HOME}/jars\")).toArray.map(_.toString)
@@ -14,4 +14,6 @@
   interp.load.cp(jars)
   import \$ivy.\`org.apache.hadoop:hadoop-client:${HADOOP_VERSION}\`
   import \$ivy.\`sh.almond::almond-spark:${ALMOND_VERSION}\`
+  import org.apache.log4j.{Level, Logger}
+  Logger.getLogger(\"org\").setLevel(Level.OFF)
   " 
