062fe576ef472e3263c82c88fe0b045b5fc8db3f
Konrad Malik <konrad.malik@gmail.com>
2019-11-10 15:59:10 +0100

    maybe fixed hadoop lack of http

--- scripts/almond-install.sh
+++ scripts/almond-install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-./almond --install --global 
+# need to override hadoop to support http!
+# we will filter out 'curator' jars as they are then reimported with new hadoop
 
-#--predef-code " 
-#  val jars = java.nio.file.Files.list(java.nio.file.Paths.get(\"${SPARK_HOME}/jars\")).toArray.map(_.toString)
-#    .map { fname =>
-#        val path = java.nio.file.FileSystems.getDefault().getPath(fname)
-#        ammonite.ops.Path(path)
-#    }
-#  interp.load.cp(jars)
-#  import \$ivy.\`sh.almond::almond-spark:${ALMOND_VERSION}\`
-#  " 
+./almond --install --global \
+--predef-code " 
+  val jars = java.nio.file.Files.list(java.nio.file.Paths.get(\"${SPARK_HOME}/jars\")).toArray.map(_.toString)
+    .filter(_.contains(\"curator\"
+    .map { fname =>
+        val path = java.nio.file.FileSystems.getDefault().getPath(fname)
+        ammonite.ops.Path(path)
+    }
+  interp.load.cp(jars)
+  import \$ivy.\`org.apache.hadoop:hadoop-client:${HADOOP_VERSION}\`
+  import \$ivy.\`sh.almond::almond-spark:${ALMOND_VERSION}\`
+  " 
