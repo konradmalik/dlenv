97921a95c5d0cf78f44dd7b0198fcb37f9d7af02
Konrad Malik <konrad.malik@gmail.com>
2019-11-10 20:57:42 +0100

    Revert "just checking without prefdef"
    This reverts commit 2fdc2a321d6f660f4c463eaa712789af1b0472ec.

--- scripts/almond-install.sh
+++ scripts/almond-install.sh
@@ -5,15 +5,15 @@
 # .filter(_.contains(\"curator\"))
 # also disable logs for 'org' to not pollute all notebook
 ./almond --install --global \
-#--predef-code " 
-#  val jars = java.nio.file.Files.list(java.nio.file.Paths.get(\"${SPARK_HOME}/jars\")).toArray.map(_.toString)
-#    .map { fname =>
-#        val path = java.nio.file.FileSystems.getDefault().getPath(fname)
-#        ammonite.ops.Path(path)
-#    }
-#  interp.load.cp(jars)
-#  import \$ivy.\`sh.almond::almond-spark:${ALMOND_VERSION}\`
-#  import \$ivy.\`org.apache.hadoop:hadoop-client:${HADOOP_VERSION}\`
-#  import org.apache.log4j.{Level, Logger}
-#  Logger.getLogger(\"org\").setLevel(Level.OFF)
-#  " 
+--predef-code " 
+  val jars = java.nio.file.Files.list(java.nio.file.Paths.get(\"${SPARK_HOME}/jars\")).toArray.map(_.toString)
+    .map { fname =>
+        val path = java.nio.file.FileSystems.getDefault().getPath(fname)
+        ammonite.ops.Path(path)
+    }
+  interp.load.cp(jars)
+  import \$ivy.\`sh.almond::almond-spark:${ALMOND_VERSION}\`
+  import \$ivy.\`org.apache.hadoop:hadoop-client:${HADOOP_VERSION}\`
+  import org.apache.log4j.{Level, Logger}
+  Logger.getLogger(\"org\").setLevel(Level.OFF)
+  " 
